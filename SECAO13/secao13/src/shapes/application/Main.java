4f452eef98c4641c49c4be0568dfe729ce24e29b
igorengsoft <igorsouzasoares1705@gmail.com>
2026-02-03 20:36:28 -0300

    validation

--- SECAO13/secao13/src/shapes/application/Main.java
+++ SECAO13/secao13/src/shapes/application/Main.java
@@ -11,75 +11,94 @@ import java.util.ArrayList;
 public class Main {
 
     public static Scanner sc = new Scanner(System.in);
+    public static String errorMessage = "Você digitou algo inválido!";
     public static void main (String args[]) {
+
+        calcShapes();
+    }    
+
+    public static void calcShapes() {
 
         Integer quantity;
         List<Shape> Shapes = new ArrayList<Shape>();
 
-        System.out.print("Quantas formas serão? ");
-        quantity = sc.nextInt();
+        try {
 
-        for(Integer count = 1; count <= quantity; count++) {
+            System.out.print("Quantas formas serão? ");
+            quantity = sc.nextInt();
+
+            for(Integer count = 1; count <= quantity; count++) {
+
+                
+                Character shape;
+
+                System.out.print("Escolha retângulo (r) ou círculo (c): ");
+                shape = sc.next().charAt(0);
+
+                if (shape == 'r') {
+
+                    Color color;
+                    Double length;
+                    Double width;
+
+                    System.out.print("Digite a cor: ");
+                    color = Color.valueOf(sc.next().toUpperCase());
+
+                    System.out.print("Digite o comprimento: ");
+                    width = sc.nextDouble();
+
+                    System.out.print("Digite a largura: ");
+                    length = sc.nextDouble();
+
+                    Shape rectangle = new Rectangle(color, length, width);
+                    Shapes.add(rectangle);
+                }
+
+                else if (shape == 'c') {
+
+                    Color color;
+                    Double radius;
+
+                    System.out.print("Digite a cor: ");
+                    color = Color.valueOf(sc.next().toUpperCase());
+
+                    System.out.print("Digite o raio: ");
+                    radius = sc.nextDouble();
+
+                    Shape circle = new Circle(color, radius);
+                    Shapes.add(circle);
+                }
+            }
+            sc.close();
+
+            for(Shape item : Shapes) {
+
+                if (item instanceof Rectangle) {
+
+                    System.out.println("RETÂNGULO:");
+                    System.out.println(item.getColor());
+                    System.out.println(item.area());
+                    System.out.println("");
+                }
+
+                else {
+
+                    System.out.println("CÍRCULO:");
+                    System.out.println(item.getColor());
+                    System.out.println(item.area());
+                    System.out.println("");
+                }            
+            }
+
+        }
+
+        catch(Exception e) {
+
+            sc.nextLine();
+            System.out.println(errorMessage);
+            calcShapes();
 
             
-            Character shape;
-
-            System.out.print("Escolha retângulo (r) ou círculo (c): ");
-            shape = sc.next().charAt(0);
-
-            if (shape == 'r') {
-
-                Color color;
-                Double length;
-                Double width;
-
-                System.out.print("Digite a cor: ");
-                color = Color.valueOf(sc.next().toUpperCase());
-
-                System.out.print("Digite o comprimento: ");
-                width = sc.nextDouble();
-
-                System.out.print("Digite a largura: ");
-                length = sc.nextDouble();
-
-                Shape rectangle = new Rectangle(color, length, width);
-                Shapes.add(rectangle);
-            }
-
-            else if (shape == 'c') {
-
-                Color color;
-                Double radius;
-
-                System.out.print("Digite a cor: ");
-                color = Color.valueOf(sc.next().toUpperCase());
-
-                System.out.print("Digite o raio: ");
-                radius = sc.nextDouble();
-
-                Shape circle = new Circle(color, radius);
-                Shapes.add(circle);
-            }
         }
-        sc.close();
-
-        for(Shape item : Shapes) {
-
-            if (item instanceof Rectangle) {
-
-                System.out.println("RETÂNGULO:");
-                System.out.println(item.getColor());
-                System.out.println(item.area());
-                System.out.println("");
-            }
-
-            else {
-
-                System.out.println("CÍRCULO:");
-                System.out.println(item.getColor());
-                System.out.println(item.area());
-                System.out.println("");
-            }            
-        }
-    }    
+    }
 }   
